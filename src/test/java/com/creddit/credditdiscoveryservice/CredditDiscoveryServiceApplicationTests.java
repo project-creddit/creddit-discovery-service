d3b9e0426850a61a7088ad7d270f3f0351954e43
oereo <dlstpgns0406@gmail.com>
2022-04-18 14:57:51 +0900

    chore: 프로젝트 세팅

---
+++ src/test/java/com/creddit/credditdiscoveryservice/CredditDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creddit.credditdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredditDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
